95c28bd3b130b1b2b316e1dcc370c29cc303c301
Tasreeq Adams <81373672+Tasreeq-Adams@users.noreply.github.com>
2022-05-04 18:11:28 +0200

    Updates

---
+++ Project3_Tutor_App/src/test/java/za/ac/cput/repository/StudentRegisterRepositoryTest.java
@@ -0,0 +1,37 @@
+/**
+ * @author MoeGammad Tasreeq Adams
+ * Student no: 216173027
+ * Project assignment
+ */
+package za.ac.cput.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentRegisterRepositoryTest {
+
+    @Test
+    void getRepository() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
