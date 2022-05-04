bcd96bf6ed9635fef4b017fae7e90c149dee38c8
Tasreeq Adams <81373672+Tasreeq-Adams@users.noreply.github.com>
2022-05-04 18:31:25 +0200

    Update StudentRegisterRepositoryTest.java

--- Project3_Tutor_App/src/test/java/za/ac/cput/repository/StudentRegisterRepositoryTest.java
+++ Project3_Tutor_App/src/test/java/za/ac/cput/repository/StudentRegisterRepositoryTest.java
@@ -6,32 +6,53 @@
 package za.ac.cput.repository;
 
 import org.junit.jupiter.api.Test;
+import za.ac.cput.entity.StudentRegister;
+import za.ac.cput.factory.StudentRegisterFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentRegisterRepositoryTest {
+    private static StudentRegisterRepository repository = StudentRegisterRepository.getRepository();
+
+    private static StudentRegister studentRegister = StudentRegisterFactory.createStudentRegister
+            ("",
+                    "Tasreeq@email.com",
+                    "12345678",
+                    "12345678",
+                    "ADP");
+
+
 
     @Test
-    void getRepository() {
+    void a_create() {
+        StudentRegister created = repository.create(studentRegister);
+        assertEquals(studentRegister.getRegisterNumber(), created.getRegisterNumber());
+        System.out.println("Create:" + created);
     }
 
     @Test
-    void create() {
+    void b_read() {
+        StudentRegister read = repository.read(studentRegister.getRegisterNumber());
+        assertNotNull(read);
+        System.out.println("Read" + read);
     }
 
     @Test
-    void read() {
+    void c_update() {
+        StudentRegister updated = new StudentRegister.Builder().copy(studentRegister)
+                .setEmail("Tashi@email.com")
+                .setPassword("123456")
+                .setPasswordCon("12345678")
+                .setCourse("Application Development")
+                .build();
+        assertNotNull(repository.update(updated));
+        System.out.println("Updated:" + updated);
     }
 
-    @Test
-    void update() {
-    }
 
     @Test
-    void delete() {
-    }
-
-    @Test
-    void getAll() {
+    void d_getAll() {
+        System.out.println("show all:");
+        System.out.println(repository.getAll());
     }
 }
