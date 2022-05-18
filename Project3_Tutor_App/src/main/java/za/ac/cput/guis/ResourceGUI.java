5aa326fa4d6d7c0cda1b121d93627d5c07756540
ICE-GLXY <220166153@mycput.ac.za>
2022-05-18 09:11:54 +0200

    Connected to Resource Database And Added Resource Gui Functionality

--- Project3_Tutor_App/src/main/java/za/ac/cput/guis/ResourceGUI.java
+++ Project3_Tutor_App/src/main/java/za/ac/cput/guis/ResourceGUI.java
@@ -6,18 +6,26 @@ package za.ac.cput.guis;
  */
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.*;
+import java.util.Objects;
 import javax.swing.*;
+
 
 public class ResourceGUI extends JFrame implements ActionListener
 {
-    JLabel lblImage, lblHeading, lblItemName, lblDescription, lblAvailability;
+    JLabel lblImage, lblHeading, lblItemName, lblDescription, lblOwner, lblAvailability;
     JComboBox cboItemName;
-    JTextField txtItemName;
+    JTextField txtOwner;
+    JTextArea txtDescription;
     JPanel panelAvailability, panelCenter, panelNorth, panelSouth;
     JRadioButton radAvailable , radUnavailable;
     ButtonGroup AvailabilityButtonGroup;
-    JButton btnNext, btnPrevious, btnExit;
+    JButton btnNext, btnExit;
     Font ft1, ft2, ft3;
+    Connection con;
+    PreparedStatement pst;
+    ResultSet rs;
+    String imageName, Availability, iName, Description, ownerContact;
 
     public ResourceGUI()
     {
@@ -38,26 +46,35 @@ public class ResourceGUI extends JFrame implements ActionListener
 
         panelSouth = new JPanel();
         panelSouth.setBackground(Color.LIGHT_GRAY);
-
-        lblImage = new JLabel(new ImageIcon("Resource.PNG"));
+        imageName = "Resource.PNG";
+        lblImage = new JLabel(new ImageIcon(imageName));
         // Images will be of the selected item
 
         lblItemName = new JLabel("Item Name: ", SwingConstants.CENTER);
         lblItemName.setFont(ft2);
-        String listOfItemName[] = {"item 1", "item 2", "item 3"};
         //combo box will link to database to show selectable items
-        cboItemName = new JComboBox(listOfItemName);
+        cboItemName = new JComboBox();
         cboItemName.setForeground(Color.white);
         cboItemName.setBackground(Color.DARK_GRAY);
 
         lblDescription = new JLabel("Description: ", SwingConstants.CENTER);
         lblDescription.setFont(ft2);
-        txtItemName = new JTextField(20);
         // Descriptions of the various different items will change as an item is selected
-        txtItemName = new JTextField("Description about the item.");
-        txtItemName.setEditable(false);
-        txtItemName.setForeground(Color.white);
-        txtItemName.setBackground(Color.DARK_GRAY);
+        txtDescription = new JTextArea();
+        txtDescription.setEditable(false);
+        txtDescription.setLineWrap(true);
+        txtDescription.setWrapStyleWord(true);
+        txtDescription.setForeground(Color.white);
+        txtDescription.setBackground(Color.DARK_GRAY);
+
+        lblOwner = new JLabel("Owner's Contact: ", SwingConstants.CENTER);
+        lblOwner.setFont(ft2);
+        txtOwner = new JTextField(20);
+        // Descriptions of the various different items will change as an item is selected
+        txtOwner = new JTextField();
+        txtOwner.setEditable(false);
+        txtOwner.setForeground(Color.white);
+        txtOwner.setBackground(Color.DARK_GRAY);
 
         lblAvailability = new JLabel("Availability: ");
         lblAvailability.setFont(ft2);
@@ -70,19 +87,14 @@ public class ResourceGUI extends JFrame implements ActionListener
         AvailabilityButtonGroup.add(radAvailable);
         AvailabilityButtonGroup.add(radUnavailable);
         panelAvailability.setLayout(new GridLayout(1, 2));
-        radAvailable.setSelected(true);
         panelAvailability.add(radAvailable);
         panelAvailability.add(radUnavailable);
 
-        btnNext = new JButton("Next");
+
+        btnNext = new JButton("Update");
         btnNext.setForeground(Color.white);
         btnNext.setBackground(Color.DARK_GRAY);
         btnNext.setFont(ft1);
-
-        btnPrevious = new JButton("Previous");
-        btnPrevious.setForeground(Color.white);
-        btnPrevious.setBackground(Color.DARK_GRAY);
-        btnPrevious.setFont(ft1);
 
         btnExit = new JButton("Exit");
         btnExit.setForeground(Color.white);
@@ -91,8 +103,23 @@ public class ResourceGUI extends JFrame implements ActionListener
 
     }
 
+    public void updateItemName()
+    {
+        try {
+            pst = con.prepareStatement("select * from resources");
+            rs = pst.executeQuery();
+            while(rs.next()){
+                cboItemName.addItem(rs.getString("Item_Name"));
+            }
+        } catch (Exception e) {
+        }
+    }
+
     public void setGUI()
     {
+        connect();
+        updateItemName();
+
         panelCenter.setLayout(new GridLayout(4, 7));
         panelNorth.setLayout(new GridLayout(1, 1));
 
@@ -102,7 +129,10 @@ public class ResourceGUI extends JFrame implements ActionListener
         panelCenter.add(cboItemName);
 
         panelCenter.add(lblDescription);
-        panelCenter.add(txtItemName);
+        panelCenter.add(txtDescription);
+
+        panelCenter.add(lblOwner);
+        panelCenter.add(txtOwner);
 
         panelCenter.add(lblImage);
 
@@ -110,11 +140,9 @@ public class ResourceGUI extends JFrame implements ActionListener
         panelCenter.add(panelAvailability);
 
         panelSouth.add(btnNext);
-        panelSouth.add(btnPrevious);
         panelSouth.add(btnExit);
 
         btnNext.addActionListener(this);
-        btnPrevious.addActionListener(this);
         btnExit.addActionListener(this);
 
         this.add(panelNorth, BorderLayout.NORTH);
@@ -129,14 +157,53 @@ public class ResourceGUI extends JFrame implements ActionListener
     @Override
     public void actionPerformed(ActionEvent e)
     {
-        if (e.getActionCommand().equals("Next"))
+        if (e.getActionCommand().equals("Update"))
         {
-            System.exit(0);
-        }
+            try {
 
-        if (e.getActionCommand().equals("Previous"))
-        {
-            System.exit(0);
+                iName = (String) cboItemName.getSelectedItem();
+                pst = con.prepareStatement("select * from resources where Item_Name = ?");
+                pst.setString(1, iName);
+                rs = pst.executeQuery();
+
+                if(rs.next()==true)
+                {
+                    Description = rs.getString("Description");
+                    ownerContact = rs.getString("Owner_Contact");
+                    Availability = rs.getString("Availability");
+//                    imageName = rs.getString("ImageID");
+
+
+
+                    txtDescription.setText(Description);
+                    txtOwner.setText(ownerContact);
+//                    lblImage.setText(imageName);
+
+                    if(Availability.equals("true")){
+                        radAvailable.setSelected(true);
+                    }   else {
+                        radAvailable.setSelected(false);
+                    }
+                    if(Availability.equals("false")){
+                        radUnavailable.setSelected(true);
+                    }   else {
+                        radUnavailable.setSelected(false);
+                    }
+                }
+
+                else
+                {
+                    txtDescription.setText("");
+                    txtOwner.setText("");
+                    JOptionPane.showMessageDialog(null,"Invalid Item Name selected");
+
+                }
+            }
+
+            catch (SQLException ex)
+            {
+                ex.printStackTrace();
+            }
         }
 
         else if (e.getSource() == btnExit)
@@ -144,10 +211,28 @@ public class ResourceGUI extends JFrame implements ActionListener
             System.exit(0);
         }
     }
-//    public static void main(String[] args)
-//   {
- //       new ResourceGUI().setGUI();
- //   }
 
- }
+    public void connect()
+    {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/tutorapplication", "root","");
+            System.out.println("Database Connection Success");
+        }
+        catch (ClassNotFoundException ex)
+        {
+            ex.printStackTrace();
+        }
+        catch (SQLException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
 
+    public static void main(String[] args)
+    {
+        new ResourceGUI().setGUI();
+
+    }
+
+}
