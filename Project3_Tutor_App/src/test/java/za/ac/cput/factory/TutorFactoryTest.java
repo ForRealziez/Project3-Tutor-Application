4e746f7b313ef077a5be28c9f52a99a18bf2461b
Lelihle <214258041@mycput.ac.za>
2022-05-19 18:18:55 -0700

    Did testing for Booking and Tutor Factory classes

--- Project3_Tutor_App/src/test/java/za/ac/cput/factory/TutorFactoryTest.java
+++ Project3_Tutor_App/src/test/java/za/ac/cput/factory/TutorFactoryTest.java
@@ -2,11 +2,16 @@ package za.ac.cput.factory;
 
 import org.junit.jupiter.api.Test;
 
+import za.ac.cput.entity.Tutor;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TutorFactoryTest {
 
     @Test
     void createTutor() {
+        Tutor tutor= TutorFactory.createTutor("lihlegazi17","214258041@mycput.ac.za","Lelihle","Gazi","214258041","2022-17-03");
+        assertNotNull(tutor);
+
     }
 }
