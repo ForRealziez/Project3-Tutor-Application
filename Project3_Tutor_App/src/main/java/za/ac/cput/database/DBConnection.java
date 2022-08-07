b8f98b3d60fc6d238f2936de3b108b51877cda6a
Tasreeq Adams <81373672+Tasreeq-Adams@users.noreply.github.com>
2022-08-07 16:08:43 +0200

    Changes made to project assignment
    Re created GUI using SWING for student login and register;
    trying to establish a connection to database

---
+++ Project3_Tutor_App/src/main/java/za/ac/cput/database/DBConnection.java
@@ -0,0 +1,70 @@
+/**
+ * @author Moegammad Tasreeq Adams
+ * Student no: 216173027
+ * Project assignment
+ */
+package za.ac.cput.database;
+import java.sql.*;
+
+
+public class DBConnection {
+     protected Connection con;
+     protected void setConnecion() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        con= DriverManager.getConnection("jdbc:mysql://localhost/Demo","root","123");
+        con = con;
+    }
+}
+
+
+
+//Connection con = null;
+/*
+*try{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        con = DriverManager.getConnection("jdbc:mysql://localhost/Demo","root","");
+    } catch (Exception ex) {
+        System.out.println("there was a error connecting to db.");
+    }
+    return con;
+    
+    protected Connection _connection;
+    public BaseConnection() throws SQLException, ClassNotFoundException {
+        setConnecion();
+    }
+*/
+
+
+   
+
+
+
+
+
+
+
+
+/**
+ *public class DBConnection {
+    static final String DB_URL = "jdbc:mysql://localhost/students";
+    static final String USER = "root";
+    static final String PASS = "root";
+    
+    public static Connection connectDB(){
+    Connection conn = null;
+    try{
+        //register jdbc driver ,mot required for newer verion of jdk
+        Class.forName("com.mysql.jdbc.Driver");
+        //open a connection
+        conn = DriverManager.getConnection(DB_URL,USER,PASS);
+        return conn;
+        
+    }catch(Exception ex){
+        System.out.println("there was a error connecting to db.");
+        return null;
+    }
+    }    
+}
+ * @author Tasreeq
+ */
+
